b274b885009ec73f1240b4b4b0ad960e00866469
ishan-26 <125141868+ishan-26@users.noreply.github.com>
2024-06-29 13:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,13 @@ load_dotenv()  ## load all the environment variables
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
-## Function to load Google Gemini Pro Vision API And get response
+def get_gemini_response(input_prompt, image):
+    # ... (your existing code)
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input_prompt, image[0]])
 
-def get_gemini_repsonse(input,image,prompt):
- model = genai.GenerativeModel(model_name="gemini-1.5-flash",generation_config=generation_config,)
- response=model.generate_content([input,image[0],prompt])
- return response.text
-
+    text = response.text
+    return text
 
 def input_image_setup(uploaded_file):
     # check if file uploaded or not
@@ -92,7 +85,7 @@ Only respond if the image pertains to the food items else respond with not appro
 
 if submit:
     image_data = input_image_setup(uploaded_file)
-    response = get_gemini_response(input_prompt,image_data,input)
+    response = get_gemini_response(input_prompt,image_data)
 
     st.subheader("Your dish summary:")
     st.write(response)
